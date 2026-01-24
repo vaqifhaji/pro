f01a544a3ae67639834bbc0c87ff621dc615f120
vaqifhaji <vaqif.haji@gmail.com>
2026-01-24 19:48:25 +0400

    a

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import homepage , about
+from .views import *
+from core import views
 urlpatterns = [
-    path('', homepage, name = 'home'),
-    path('about.html', about, name = 'about')
+    path('', views.homepage, name='home'),
+    path('about/', views.about, name = 'about'),
+    path('shop/', views.shop, name = 'shop'),
+    path('faq/', views.faq, name = 'faq'),
+    path('contact/', views.contact, name = 'contact'),
+    path('blog/',  views.blog, name = 'blog'),
+    path('checkout/', views.checkout, name = 'checkout'),
+    path('cart/', views.cart, name = 'cart'),
+    path('shop-left-sidebar.html', views.shop_left_sidebar, name='shop_left_sidebar'),
+    path('wishlist.html', views.wishlist, name='wishlist'),
+    path('single-product.html', views.single_product, name='single_product'),
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,21 @@ def homepage (request):
     return render(request, 'index.html') 
 def about (request):
     return render(request, 'about.html')
+def shop (request):
+    return render(request, 'shop.html')
+def faq (request):
+    return render(request, 'faq.html')
+def contact (request):  
+    return render(request, 'contact.html')
+def cart (request):
+    return render(request, 'cart.html')
+def checkout (request):
+    return render(request, 'checkout.html')
+def blog (request):
+    return render(request, 'blog.html') 
+def shop_left_sidebar (request):
+    return render(request, 'shop-left-sidebar.html')
+def wishlist (request):
+    return render(request, 'wishlist.html')
+def single_product (request):
+    return render(request, 'single-product.html')
